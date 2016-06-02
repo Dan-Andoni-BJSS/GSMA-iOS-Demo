cae16eb38da814ae233377494a7019f247d3e712
Dan Andoni <d.andoni@rosoftlab.net>
2016-06-02 13:50:04 +0100

    Initial setup

--- CodeReview/Constants.swift
+++ CodeReview/Constants.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+let kClientKey : String = "a25e91c5-ab1e-4f9e-9fd9-4cd1482c0a48"
+//private let kClientKey : String = "e058eeb3-8813-417e-b258-4a02729dcf41"
+let kClientSecret : String = "1d95d440-49fe-455e-8fd4-b5903b8c78ec"
+//private let kClientSecret : String = "235c44a5-51e0-44b1-92e9-e425206206d8"
+let kSandboxEndpoint : String = "http://discovery.sandbox2.mobileconnect.io/v2/discovery"
+//private let kSandboxEndpoint : String = "http://discovery.dev.sandbox.mobileconnect.io/v2/discovery"
+
+let kRedirectURL : String = "http://test.test.com"
+
+//private let kRedirectURL : String = "http://localhost:8001/mobileconnect.html"
